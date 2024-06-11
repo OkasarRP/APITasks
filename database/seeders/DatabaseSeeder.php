5fb04d40cdfca374de75f62b478934941bb1fe37
OkasarRP <oskyrp2000@gmail.com>
2024-06-11 19:02:29 +0200

    almost done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UserSeeder::class,
-            TaskSeeder::class,
+            TaskSeeder::class
 
         ]);
     }
